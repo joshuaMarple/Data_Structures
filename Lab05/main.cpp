3f2d22808cdf4a1f2fcb977d747e0002cb37156f
Joshua Marple <marpljos@gmail.com>
2014-10-05 18:20:36 -0500

    almost all of lab 5 done, need to do level order traversal

---
+++ Lab05/main.cpp
@@ -0,0 +1,56 @@
+#include "Binary_search_tree.cpp"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+	
+	Binary_search_tree* tree = new Binary_search_tree();
+	
+    std::ifstream infile;
+    infile.open ("data.txt");
+    int i = 0;
+    std::string line;
+    while(std::getline(infile, line)){
+        std::stringstream  lineStream(line);
+        int value;
+        while(lineStream >> value){
+        	tree->insert(value);
+        }
+    }
+    infile.close();
+	
+	std::cout << "----------------------\n" << "1- insert\n" << "2- remove\n" << "3- delete_min\n" << "4- delete_max\n"
+		<< "5- preorder\n" << "6- inorder\n" << "7- levelorder\n" << "8- exit\n";
+	int input;
+	std::cin >> input;
+	
+	while(input != 8){
+		if(input == 1){
+			std::cout << "Which number would you like to insert?\n";
+			std::cin >> input;
+			tree->insert(input);
+		}else if(input == 2){
+			std::cout << "Which number would you like to remove\n";
+			std::cin >> input;
+			tree->remove(input);
+		}else if(input == 3){
+			tree->delete_min();
+		}else if(input == 4){
+			tree->delete_max();
+		}else if(input == 5){
+			tree->preorder();
+		}else if(input == 6){
+			tree->inorder();
+		// }else if(input == 7){
+		// 	tree->levelorder();
+		}
+		std::cout << "\n\n----------------------\n" << "1- insert\n" << "2- remove\n" << "3- delete_min\n" << "4- delete_max\n"
+			<< "5- preorder\n" << "6- inorder\n" << "7- levelorder\n" << "8- exit\n";
+		std::cin >> input;
+	}
+		
+	
+	return 0;
+}
